1cf01992626477149df8a0b02a250f64cd08261b
eeedddiii <eeedddiii82@gmail.com>
2025-01-25 14:27:31 +0900

    프로젝트 초기 설정 - edi

---
+++ src/test/java/com/example/ieumai_backend/IeumaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ieumai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeumaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
