3ae62cf6ebbbf32babf50ba6f3d99499e9ffdd2c
EC2 Default User <ec2-user@ip-172-26-7-137.ap-northeast-2.compute.internal>
2025-04-10 10:47:04 +0000

    chore: improve build-script

--- build-script.sh
+++ build-script.sh
@@ -3,7 +3,7 @@
 ARTIFACT_DIR="./build/libs"
 PORT=8080
 
-# 기본 브랜치 설정
+# 0. 기본 브랜치 설정
 if [ -z "$BRANCH_NAME" ]; then
   BRANCH_NAME="main"
 fi
@@ -12,9 +12,8 @@ echo "=== Starting Deployment Script ==="
 
 set -e
 
-# 0. 기존 서버 종료
+# 기존 서버 죽이기
 EXIST_PID=$(lsof -t -i :$PORT)
-
 if [ -n "$EXIST_PID" ]; then
   echo ">>> Killing existing process on port $PORT (PID: $EXIST_PID)"
   kill -9 $EXIST_PID
@@ -23,32 +22,28 @@ else
   echo ">>> No existing process on port $PORT"
 fi
 
-# 1. Git Pull
+# Git pull
 echo "[1/3] Pulling latest code from Git repository (${BRANCH_NAME} branch)..."
 git pull origin ${BRANCH_NAME} || true
-# 2. Gradle Build
-echo "[2/3] Building the project using Gradle..."
 
+# Build
+echo "[2/3] Building the project using Gradle..."
 if [ ! -x ./gradlew ]; then
   echo "Warning: ./gradlew does not have execute permissions. Granting permissions."
   chmod +x ./gradlew
 fi
-
 ./gradlew --stop
 ./gradlew build
 
-# 3. 실행할 JAR 찾기
+# Jar 실행
 echo "[3/3] Finding and executing the built artifact (JAR file)..."
 JAR_FILE=$(ls -t ${ARTIFACT_DIR}/*.jar | grep -v 'plain' | head -n 1)
-
 if [ -z "${JAR_FILE}" ]; then
   echo "Error: Could not find a JAR file in the ${ARTIFACT_DIR} directory."
   exit 1
 fi
 
 echo ">>> Executing JAR file: ${JAR_FILE}"
-
-# 4. nohup 으로 백그라운드 실행
 rm -f nohup.out
 nohup java -jar "${JAR_FILE}" &
 sleep 3
